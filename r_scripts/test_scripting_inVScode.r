a601dc829051731058e0c81273ad679d2bae615b
Rolf Søkilde <rolf.soekilde@gmail.com>
2024-10-03 12:58:11 +0200

    added a theme in rstudio

--- r_scripts/test_scripting_inVScode.r
+++ r_scripts/test_scripting_inVScode.r
@@ -3,6 +3,8 @@
 library(tidyverse)
 # make a tibble
 df <- tibble(a = 1:10, b = 10:1)
-ggplot(df, aes(a, b)) + 
+p1 <- ggplot(df, aes(a, b)) + 
     geom_point()  # plot the tibble
+# edited in Rstudio
+p1 + theme_bw()
 # end of script
